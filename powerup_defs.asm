5e358e44844cc4d697c75072d5e1b69d9ef3b54b
TheLX5 <luisyuregi@gmail.com>
2016-06-10 13:13:23 -0700

    removed unnecessary defines

--- powerup_defs.asm
+++ powerup_defs.asm
@@ -35,9 +35,6 @@ endif
 !clear_7E2000		= 1	;Clear Mario GFX from RAM. Needs to be 0 if using Dynamic Z or the Mode 7 Game Over patch.
 				;1 = enable, 0 = disable
 
-!default_interaction	= 1	;Includes Sonikku's Custom Default Interaction patch.
-				;1 = enable, 0 = disable
-
 !boomerang_inserted	= 0	;Set to 1 if using my Boomerang Suit
 !iceball_inserted	= 0	;Set to 1 if using my Ice Flower
 !raccoon_inserted	= 0	;Set to 1 if using my Raccoon or Tanooki powerups.
