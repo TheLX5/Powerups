86a20dea2c14f83901d66f2070e5f792242e5ed1
TheLX5 <luisyuregi@gmail.com>
2019-01-05 14:55:23 -0700

    Fixed 5th tile garbage when instantly dying.

--- powerups_files/instant_kill_flag.asm
+++ powerups_files/instant_kill_flag.asm
@@ -1,5 +1,16 @@
 if !better_powerdown == 0
 instant_kill:
+	lda #$10
+	tsb $78
+	lda !slippery_flag_backup
+	beq +
+	sta $86
++	
+	lda #$00
+	sta !slippery_flag_backup
+	sta !extra_tile_flag
+	sta !cape_settings
+
 	lda !insta_kill_flag
 	beq .no_insta_kill
 	pla
@@ -10,13 +21,5 @@ instant_kill:
 if !disable_drop_item == 0
 	jsl $028008|!base3
 endif
-	lda !slippery_flag_backup
-	beq +
-	sta $86
-+	
-	lda #$00
-	sta !slippery_flag_backup
-	sta !extra_tile_flag
-	sta !cape_settings
 	rtl
 endif
