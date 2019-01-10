08f6ac1b6bab45d297a70419bbae668f34b1c49e
TheLX5 <luisyuregi@gmail.com>
2019-01-10 11:58:17 -0700

    Forgot this file to expand the max number of powerups.

--- powerups_files/powerup_misc_data/gfx_index.asm
+++ powerups_files/powerup_misc_data/gfx_index.asm
@@ -6,20 +6,20 @@ GFXData:
 	;mario gfx
 	db $00,$01,$01,$01,$02,$03,$04,$05		; powerups 0 - 7
 	db $06,$01,$01,$07,$01,$08,$09,$0A		; powerups 8 - F
-	db $01,$01,$0B					; powerups 10 - 12
+	db $01,$01,$0B,$01,$01,$01,$01,$01		; powerups 10 - 17
 
 	;luigi gfx
 	db $00,$01,$01,$01,$02,$03,$04,$05		; powerups 0 - 7
 	db $06,$01,$01,$07,$01,$08,$09,$0A		; powerups 8 - F
-	db $01,$01,$0B					; powerups 10 - 12
+	db $01,$01,$0B,$01,$01,$01,$01,$01		; powerups 10 - 12
 
 ExtraGFXData:
 	;mario extra gfx
 	db $FF,$FF,$00,$FF,$FF,$FF,$01,$01		; powerups 0 - 7
 	db $FF,$FF,$FF,$FF,$FF,$FF,$02,$FF		; powerups 8 - F
-	db $FF,$03,$04					; powerups 10 - 12
+	db $FF,$03,$04,$FF,$FF,$FF,$FF,$FF		; powerups 10 - 12
 
 	;luigi extra gfx
 	db $FF,$FF,$00,$FF,$FF,$FF,$01,$01		; powerups 0 - 7
 	db $FF,$FF,$FF,$FF,$FF,$FF,$02,$FF		; powerups 8 - F
-	db $FF,$03,$04					; powerups 10 - 12
+	db $FF,$03,$04,$FF,$FF,$FF,$FF,$FF		; powerups 10 - 12
