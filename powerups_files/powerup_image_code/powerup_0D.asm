4544464d7d109ba4a9baa8ef71bedc1f49af08b1
TheLX5 <luisyuregi@gmail.com>
2019-01-18 20:09:05 -0700

    Fixed some frames in Penguin Suit

--- powerups_files/powerup_image_code/powerup_0D.asm
+++ powerups_files/powerup_image_code/powerup_0D.asm
@@ -3,18 +3,23 @@
 ;;;;;;;;;;;;;;;;;;;
 
 .image
+	lda $75
+	beq .no_water
 	lda !flags
 	bne .sliding
 .return
-	lda $75
-	eor #$01
-	ora $71
+	lda $71
 	ora $13ED|!base2
 	ora $1493|!base2
 	ora $148F|!base2
 	bne .no_water
 	lda $149C|!base2
 	beq .override
+	lda $75
+	bne +
+	lda #$17
+	bra .write
++	
 	lda #$2C
 	bra .write
 .override
@@ -28,7 +33,5 @@
 .sliding
 	lda $1891|!base2
 	bne .return
-;	lda #%00011101
-;	tsb $78
 	lda #$16
 	bra .write
