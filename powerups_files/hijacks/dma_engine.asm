06061cea6268906f7420d8a002b2f7839fca101f
TheLX5 <luisyuregi@gmail.com>
2020-02-07 17:09:10 -0700

    Stopped main DMA hijack reinserting its RTS every time the main patch was reinserted

--- powerups_files/hijacks/dma_engine.asm
+++ powerups_files/hijacks/dma_engine.asm
@@ -1,6 +1,8 @@
 org $00A300|!base3			;Main DMA routines handler. Includes:
 !a	JML PlrDMA			;Projectile DMA, 8x8 DMAer and Mario ExGFX
+if read1($00A304|!base3) != $5C
 	RTS
+endif
 
 org $0FFB9C|!base3
 !a	JML skip_mario_gfx
