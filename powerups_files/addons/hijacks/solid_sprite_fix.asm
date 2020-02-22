3d8fbc9ebcb16e0b7d6d16cafc7fe50233d1c5ef
TheLX5 <luisyuregi@gmail.com>
2020-02-21 19:35:00 -0700

    Fixes mini mushroom interaction with the bonus game

--- powerups_files/addons/hijacks/solid_sprite_fix.asm
+++ powerups_files/addons/hijacks/solid_sprite_fix.asm
@@ -1,2 +1,5 @@
 org $01B4C0|!base3
 	jml fix_mini_solid
+
+org $01DE61|!base3
+	jml fix_bonus_game
--- powerups_files/addons/solid_sprite_fix.asm
+++ powerups_files/addons/solid_sprite_fix.asm
@@ -8,3 +8,15 @@ fix_mini_solid:
 	jml $01B4C4|!base3
 .original
 	jml $01B4C6|!base3
+
+fix_bonus_game:
+	lda #$F0
+	ldy $19
+	cpy #!mini_mushroom_powerup_num
+	beq .original
+	lda #$F4
+	cpy #$00
+	beq .original
+	lda #$00
+.original
+	jml $01DE69|!base3
